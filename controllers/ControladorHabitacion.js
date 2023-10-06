b482068fc1863d80bad678dd4d9c0b83a4ab0f69
AnselmoOrtega <127802328+lfkanselmo@users.noreply.github.com>
2023-10-06 08:25:48 -0500

    avance en la creacion de la API

--- controllers/ControladorHabitacion.js
+++ controllers/ControladorHabitacion.js
@@ -1,9 +1,101 @@
 export class ControladorHabitacion{
     constructor(){}
 
-    buscarTodas(request,response){}
-    buscarPorId(request,response){}
-    modificar(request,response){}
-    registrar(request,response){}
-    eliminar(request,response){}
+    buscarTodas(request,response){
+        try{
+            //1. hay que recibir datos
+            //2. intentare conectarme a la BD
+            //3. envio la respuesta
+            response.status(200).json({
+                "estado": true,
+                "mensaje": "exito buscando las habitaciones",
+                "datos": "aca van los datos consultados en la base de datos"
+            })
+        }catch(error){
+            response.status(400).json({
+                "estado": false,
+                "mensaje":"fallamos buscando las habitaciones"+error,
+                "datos": null
+            })
+        }
+    }
+    buscarPorId(request,response){
+        try {
+            //1. hay que recibir datos(si)
+            let id=request.params.id
+            //2. con el id que mando el cliente busco la habitacion en BD
+            //3. respondo al cliente
+             response.status(200).json({
+               estado: true,
+               mensaje: "exito buscando las habitaciones",
+               datos: "aca van los datos consultados en la base de datos",
+             });
+        }catch(error){
+             response.status(400).json({
+               estado: false,
+               mensaje: "fallamos buscando las habitaciones"+ error,
+               datos: null,
+             });
+        }
+    }
+    modificar(request,response){
+        try {
+            //1. hay que recibir datos(SI)
+            let id=request.params.id
+            let datosModificar=request.body;
+            //2. modificar en la base de datos
+            //3. enviar respuesta 
+            response.status(200).json({
+              estado: true,
+              mensaje: "exito buscando las habitaciones",
+              datos: null
+            });
+        } catch (error) {
+            response.status(400).json({
+              estado: false,
+              mensaje: "fallamos buscando las habitaciones"+ error,
+              datos: null,
+            });
+        }
+    }
+    registrar(request,response){
+        try {
+          //1. hay que recibir datos(SI)
+          let datosRegistrar = request.body;
+          //2. guardelos en BD
+          //3. responda
+          response.status(200).json({
+            estado: true,
+            mensaje: "exito buscando las habitaciones",
+            datos: null,
+          });
+        } catch (error) {
+            response.status(400).json({
+              estado: false,
+              mensaje: "fallamos buscando las habitaciones"+error,
+              datos: null,
+            });
+        }
+       
+    }
+    eliminar(request,response){
+        try {
+            //1.hay que recibir datos (si)
+            let id=request.params.id
+            //2.eliminelo en la base de datos
+            //3. responda
+             response.status(200).json({
+               estado: true,
+               mensaje: "exito buscando las habitaciones",
+               datos: null,
+             });
+
+        } catch (error) {
+            response.status(400).json({
+              estado: false,
+              mensaje: "fallamos buscando las habitaciones" + error,
+              datos: null,
+            });
+        }
+    }
 }
--- routes/rutasHoteles.js
+++ routes/rutasHoteles.js
@@ -6,18 +6,14 @@ import express from 'express';
 export let rutasAPI=express.Router()
 
 //Aca pone sus EndPoints
-rutasAPI.post('/api/habitaciones', function (req, res) {
-    res.send("estoy guardando una habitacion");
-  });
-rutasAPI.get("/api/habitaciones", function (req, res) {
-    res.send("estoy bucando todas las habitaciones");
-  });
-rutasAPI.get("/api/habitacion", function (req, res) {
-    res.send("estoy  buscando una habitacion");   
-  });
-rutasAPI.put("/api/habitaciones", function (req, res) {
-    res.send("estoy modificando una habitacion");
-  });
-rutasAPI.delete("/api/habitaciones", function (req, res) {
-    res.send("estoy eliminando una habitacion");
-  });
+rutasAPI.post('/api/habitaciones');
+rutasAPI.get("/api/habitaciones");
+rutasAPI.get("/api/habitacion/:id");
+rutasAPI.put("/api/habitaciones/:id",);
+rutasAPI.delete("/api/habitaciones/:id");
+//Reservas
+rutasAPI.post("/api/reservas");
+rutasAPI.get("/api/reservas");
+rutasAPI.get("/api/reserva/:id");
+rutasAPI.put("/api/reservas/:id");
+rutasAPI.delete("/api/reservas/:id");
